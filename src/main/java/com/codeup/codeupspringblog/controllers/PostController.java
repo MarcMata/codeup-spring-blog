c320725496a75df5367ce3a4ee6dff3e33c549c4
Marc Mata <marcmata26@gmail.com>
2023-05-25 10:42:34 -0500

    completed exercise for views

--- src/main/java/com/codeup/codeupspringblog/controllers/PostController.java
+++ src/main/java/com/codeup/codeupspringblog/controllers/PostController.java
@@ -1,20 +1,36 @@
 package com.codeup.codeupspringblog.controllers;
 
+import com.codeup.codeupspringblog.models.Post;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class PostController {
     @GetMapping("/posts")
-    @ResponseBody
-    public String allPosts() {
-        return "posts index page";
+    public String allPosts(Model model) {
+        List<Post> allPosts = new ArrayList<>();
+        Post post1 = new Post("Test title1", "lorem ipsum dolor sit amet");
+        Post post2 = new Post("Test title2", "lorem ipsum dolor sit amet");
+        System.out.println(post1);
+        System.out.println(post2);
+        allPosts.add(post1);
+        allPosts.add(post2);
+        model.addAttribute("posts", allPosts);
+
+        return "posts/index";
     }
 
     @GetMapping("/posts/{id}")
-    @ResponseBody
-    public String viewIndividualPost(@PathVariable int id) {
-        return "view an individual post";
+    public String viewIndividualPost(@PathVariable int id, Model model) {
+        Post post = new Post("Test Title", "lorem ipsum dolor sit amet");
+        System.out.println(post.getTitle());
+        System.out.println(post.getBody());
+        model.addAttribute("post", post);
+        return "posts/show";
     }
 
     @GetMapping("/posts/create")
